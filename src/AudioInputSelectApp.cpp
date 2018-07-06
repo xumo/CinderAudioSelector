ceb49ac7c491cf2ac0a0e597236104949ec49c89
Rodrigo Torres <studio@rodrigotorres.net>
2018-07-06 13:35:42 +0100

    Clean up

--- src/AudioInputSelectApp.cpp
+++ src/AudioInputSelectApp.cpp
@@ -7,7 +7,8 @@
 *	@xumerio
 *	2018	
 *
-*	Feel free to use it as you like.
+*	Feel free to use it as you like. 
+*	This code is not ready for production in any way, so I'm not resposible for any further use of it.
 *	
 *	It uses this librearie, so check the licence details of them i you need to 
 *	Cinder https://libcinder.org
@@ -42,14 +43,17 @@ using namespace std;
 class AudioInputSelectApp : public App {
   public:
 	void setup() override;
-	void mouseDown( MouseEvent event ) override;
-	void update() override;
-	void draw() override;
+	
 	void loadPing();
 	void connectOutput();
 	void ping();
 	void selectDevice(string deviceStr);
 	void changeOutput(string output);
+	void updateUi();
+
+
+	void update() override;
+	void draw() override;
 
 	audio::InputDeviceNodeRef		mInputDeviceNode;
 	audio::MonitorSpectralNodeRef	mMonitorSpectralNode, mSpectral;
@@ -82,9 +86,11 @@ void AudioInputSelectApp::setup()
 	for (auto audioDevice : ci::audio::Device::getOutputDevices()) {
 		mAudioOutputDevices.push_back(audioDevice->getName());
 	};
+
 	mSelectedOutput = "Not selected";
 	loadPing();
 
+	//Bring focus to the opengl window
 #if defined(CINDER_MSW)
 	// at top of file, add #include <windows.h> (also within a CINDER_MSW block)
 	auto nativeWindow = static_cast<HWND>(getWindow()->getNative());
@@ -93,82 +99,6 @@ void AudioInputSelectApp::setup()
 #endif // CINDER_MSW
 }
 
-void AudioInputSelectApp::mouseDown( MouseEvent event )
-{
-}
-
-void AudioInputSelectApp::update()
-{
-	
-	ImGuiWindowFlags_ winFlags;
-
-	{
-		ui::ScopedWindow window("Audio Input", vec2(500, 263), 0.85);
-		static int item2 = -1;
-		ui::Text("Select an audio input");
-		//ImGui::ListBox("Devices", &item2, vector_getter, static_cast<void*>(&mDevices), mDevices.size());
-		ui::Combo("Audio Input Devices", &item2, mDevices, mDevices.size());
-
-		bool select = ImGui::Button("Select");
-		if (select) {
-			if (item2 >= 0 && item2 < mDevices.size()) {
-				selectDevice(mDevices.at(item2));
-			}
-		}
-
-		ImGui::Dummy(ci::vec2(20, 20));
-		static int itemOut = -1;
-		ui::Text("Select an audio output");
-		ui::Combo("Audio Output Devices", &itemOut, mAudioOutputDevices, mAudioOutputDevices.size());
-
-		bool selectOut = ImGui::Button("Select Audio Output");
-		if (selectOut) {
-			if (itemOut >= 0 && itemOut < mAudioOutputDevices.size()) {
-				
-				mSelectedOutput = mAudioOutputDevices[itemOut];
-				changeOutput(mSelectedOutput);
-			}
-		}
-		ui::Text("Selected output");
-		ui::Text(mSelectedOutput.c_str());
-
-		if (ui::Button("Ping")) {
-			ping();
-		}
-	}
-	
-	{
-		//ui::ShowTestWindow();
-	}
-
-	
-	for (AudioOutputRef &output : mOutputs) {
-		
-		output->update();
-		ui::ScopedWindow window(output->getName(), vec2(440, 120));
-		//ImGui::Columns(0);
-		ImGui::Columns(1, "statscols", false);
-		ImVec2 size = ImGui::GetItemRectSize();
-		int i = 0;
-		ImGui::PushItemWidth(380);
-		for (auto &spectrum : output->spectrums()) {
-			i++;
-			ui::PlotHistogram( i == 1 ? "left":"right", spectrum.data(), spectrum.size(), 0, NULL, 0.0f, 0.001f, ImVec2(0, 40));
-		}
-		ImGui::PopItemWidth();
-		
-		//
-		//ImGui::PlotLines("Lines", output->spectrum.data(), output->spectrum.size(), 0, "avg 0.0", 0.0f, 0.001f, ImVec2(0, 80));
-	}
-}
-
-
-
-void AudioInputSelectApp::draw()
-{
-	gl::clear( Color( 0, 0, 0 ) ); 
-
-}
 
 void AudioInputSelectApp::loadPing()
 {
@@ -186,16 +116,10 @@ void AudioInputSelectApp::loadPing()
 		ci::app::console() << "[xumo::AudioModule::loadPing] failed to load sound." << exc.what() << std::endl;
 	}
 	ci::app::console() << "[xumo::AudioModule::loadPing] laoaded" << std::endl;
-	//connectOutput();
 }
 
 void AudioInputSelectApp::connectOutput()
 {
-	if (mSoundBufferPing->isConnectedToOutput(mGain)) {
-		//mSoundBufferPing->disconnect(mGain);
-	}
-	//	mSoundBufferPing->disconnectAllOutputs();
-
 	auto ctx = ci::audio::Context::master();
 
 	if (mGain) {
@@ -219,17 +143,14 @@ void AudioInputSelectApp::ping()
 
 void AudioInputSelectApp::selectDevice(string deviceStr)
 {
-	console() << "Find audio device by name: " << deviceStr << endl;
+	console() << "[AudioInputSelectApp::selectDevice] Find audio device by name: " << deviceStr << endl;
 	ci::audio::DeviceRef device;
 	for (auto audioDevice : ci::audio::Device::getDevices()) {
-		//ci::app::console() << "device " << audioDevice->getName() << std::endl;
 		std::size_t found = audioDevice->getName().find(deviceStr);
 		if (found != std::string::npos) {
 			device = audioDevice;
 		}
 	};
-
-
 
 	bool repeated = false;
 	for (AudioOutputRef output: mOutputs) {
@@ -240,7 +161,7 @@ void AudioInputSelectApp::selectDevice(string deviceStr)
 
 	if (device && !repeated) {
 
-		console() << "Audio ouput add-->>>> " << device->getName() << endl;
+		console() << "[AudioInputSelectApp::selectDevice] Audio ouput add-->>>> " << device->getName() << endl;
 		mOutputs.push_back(AudioOutputRef(new AudioOutput(device)));
 	}
 	
@@ -252,24 +173,85 @@ void AudioInputSelectApp::changeOutput(string output)
 	ctx->disable();
 	auto device = ci::audio::Device::findDeviceByName(output);
 	if (device)
-		ci::app::console() << "[xumo::AudioModule::selectOutput] device " << device->getName() << "\n";
+		ci::app::console() << "[AudioInputSelectApp::changeOutput] device " << device->getName() << "\n";
 	ci::audio::OutputDeviceNodeRef outputDevice = ctx->createOutputDeviceNode(device);
 	
 	ctx->setOutput(outputDevice);
 	connectOutput();
-	if (mGain) {
-		//mGain->disconnectAllOutputs();
-		//mGain >> output;
-		
-		//mGain >> ctx->getOutput();
-		//ctx->enable();
+	
+}
+
+void AudioInputSelectApp::updateUi()
+{
+	ImGuiWindowFlags_ winFlags;
+
+	{
+		ui::ScopedWindow window("Audio Input", vec2(500, 263), 0.85);
+		static int item2 = -1;
+		ui::Text("Select an audio input");
+		ui::Combo("Audio Input Devices", &item2, mDevices, mDevices.size());
+
+		bool select = ImGui::Button("Select");
+		if (select) {
+			if (item2 >= 0 && item2 < mDevices.size()) {
+				selectDevice(mDevices.at(item2));
+			}
+		}
+
+		ImGui::Dummy(ci::vec2(20, 20));
+		static int itemOut = -1;
+		ui::Text("Select an audio output");
+		ui::Combo("Audio Output Devices", &itemOut, mAudioOutputDevices, mAudioOutputDevices.size());
+
+		bool selectOut = ImGui::Button("Select Audio Output");
+		if (selectOut) {
+			if (itemOut >= 0 && itemOut < mAudioOutputDevices.size()) {
+
+				mSelectedOutput = mAudioOutputDevices[itemOut];
+				changeOutput(mSelectedOutput);
+			}
+		}
+		ui::Text("Selected output");
+		ui::Text(mSelectedOutput.c_str());
+
+		if (ui::Button("Ping")) {
+			ping();
+		}
 	}
+
+
+	for (AudioOutputRef &output : mOutputs) {
+
+		output->update();
+		ui::ScopedWindow window(output->getName(), vec2(440, 120));
+		ImGui::Columns(1, "statscols", false);
+		ImVec2 size = ImGui::GetItemRectSize();
+		int i = 0;
+		ImGui::PushItemWidth(380);
+		for (auto &spectrum : output->spectrums()) {
+			i++;
+			ui::PlotHistogram(i == 1 ? "left" : "right", spectrum.data(), spectrum.size(), 0, NULL, 0.0f, 0.001f, ImVec2(0, 40));
+		}
+		ImGui::PopItemWidth();
+
+	}
+}
+
+void AudioInputSelectApp::update()
+{
+	updateUi();
+}
+
+
+
+void AudioInputSelectApp::draw()
+{
+	gl::clear(Color(0, 0, 0));
 }
 
 CINDER_APP( AudioInputSelectApp, RendererGl(RendererGl::Options().msaa(8)), [](App::Settings * settings) {
 	settings->setBorderless(false);
 	settings->setConsoleWindowEnabled(true);
-	settings->setWindowSize(1024, 768);
-	settings->setFrameRate(50);
-	settings->setTitle("Audio input selector");
+	settings->setWindowSize(600, 768);
+	settings->setTitle("Audio selector");
 })
